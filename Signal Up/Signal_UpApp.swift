1434ea232f5715248272b59b2ea936fd2f690318
v-AMusayelyan <v-amusayelyan@quantumftg.com>
2025-02-25 14:46:11 +0400

    Initial Commit

---
+++ Signal Up/Signal_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Signal_UpApp.swift
+//  Signal Up
+//
+//  Created by Aren Musayelyan on 25.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Signal_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
